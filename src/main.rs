c236764438540bb7a7c24cbbc605a8601c874f53
Noam Favier <95364866+NoamFav@users.noreply.github.com>
2024-09-02 17:52:07 +0200

    added board and pieces that works

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,35 @@
 use util::color::Color;
 use util::position::Position;
-
-mod pieces {
-    pub mod rook;
-    pub mod pawn;
-    pub mod knight;
-    pub mod bishop;
-    pub mod king;
-    pub mod queen;
-}
+use crate::util::board::Board;
+use crate::util::pieces;
 
 mod util {
     pub mod color;
     pub mod position;
     pub mod board;
+    pub mod pieces;
 }
 
 fn main() {
 
-    let rook = pieces::rook::Rook::new(Color::black(), Position::new('a', '1'));
-    let pawn = pieces::pawn::Pawn::new(Color::white(), Position::new('a', '2'));
-    let knight = pieces::knight::Knight::new(Color::black(), Position::new('a', '3'));
-    let bishop = pieces::bishop::Bishop::new(Color::white(), Position::new('a', '4'));
-    let king = pieces::king::King::new(Color::black(), Position::new('a', '5'));
-    let queen = pieces::queen::Queen::new(Color::white(), Position::new('a', '6'));
+    let _rook = pieces::Rook::new(Color::black(), Position::new('a', '1'));
+    let _pawn = pieces::Pawn::new(Color::white(), Position::new('a', '2'));
+    let _knight = pieces::Knight::new(Color::black(), Position::new('a', '3'));
+    let _bishop = pieces::Bishop::new(Color::white(), Position::new('a', '4'));
+    let _king = pieces::King::new(Color::black(), Position::new('a', '5'));
+    let _queen = pieces::Queen::new(Color::white(), Position::new('a', '6'));
 
-    println!("{:?}", rook.color);
-    println!("{:?}", rook.position);
-    println!("{:?}", rook.has_moved);
 
-    println!();
+    let mut board = Board::new();
+    println!("{}", board.render_ascii());
 
-    println!("{:?}", pawn.color);
-    print!("{:?}", pawn.position);
-    println!("{:?}", pawn.has_moved);
-
-    println!();
-
-    println!("{:?}", knight.color);
-    println!("{:?}", knight.position);
-    println!("{:?}", knight.has_moved);
-
-    println!();
-
-    println!("{:?}", bishop.color);
-    println!("{:?}", bishop.position);
-    println!("{:?}", bishop.has_moved);
-
-    println!();
-
-    println!("{:?}", king.color);
-    print!("{:?}", king.position);
-    println!("{:?}", king.has_moved);
-
-    println!();
-
-    println!("{:?}", queen.color);
-    print!("{:?}", queen.position);
-    println!("{:?}", queen.has_moved);
+    println!("{:?}", board.transform_algebraic_notation("e4"));
+    println!("{:?}", board.transform_algebraic_notation("e5"));
+    println!("{:?}", board.transform_algebraic_notation("Nf3"));
+    println!("{:?}", board.transform_algebraic_notation("Nc6"));
+    println!("{:?}", board.transform_algebraic_notation("Bb5"));
+    println!("{:?}", board.transform_algebraic_notation("a6"));
+    println!("{:?}", board.transform_algebraic_notation("Ba4+"));
 
     println!("Hello, world!");
 }
--- target/debug/deps/chest_bot-77e5b598b0126735.d
+++ target/debug/deps/chest_bot-77e5b598b0126735.d
@@ -1,14 +1,9 @@
-/Users/noamfavier/RustroverProjects/chest-bot/target/debug/deps/chest_bot-77e5b598b0126735: src/main.rs src/pieces/rook.rs src/pieces/pawn.rs src/pieces/knight.rs src/pieces/bishop.rs src/pieces/king.rs src/pieces/queen.rs src/util/color.rs src/util/position.rs src/util/board.rs
+/Users/noamfavier/RustroverProjects/chest-bot/target/debug/deps/chest_bot-77e5b598b0126735: src/main.rs src/util/color.rs src/util/position.rs src/util/board.rs src/util/pieces.rs
 
-/Users/noamfavier/RustroverProjects/chest-bot/target/debug/deps/chest_bot-77e5b598b0126735.d: src/main.rs src/pieces/rook.rs src/pieces/pawn.rs src/pieces/knight.rs src/pieces/bishop.rs src/pieces/king.rs src/pieces/queen.rs src/util/color.rs src/util/position.rs src/util/board.rs
+/Users/noamfavier/RustroverProjects/chest-bot/target/debug/deps/chest_bot-77e5b598b0126735.d: src/main.rs src/util/color.rs src/util/position.rs src/util/board.rs src/util/pieces.rs
 
 src/main.rs:
-src/pieces/rook.rs:
-src/pieces/pawn.rs:
-src/pieces/knight.rs:
-src/pieces/bishop.rs:
-src/pieces/king.rs:
-src/pieces/queen.rs:
 src/util/color.rs:
 src/util/position.rs:
 src/util/board.rs:
+src/util/pieces.rs:
