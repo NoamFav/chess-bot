2d21b7617a8d49f8de43937ce8e1ece2bfb65862
Noam Favier <95364866+NoamFav@users.noreply.github.com>
2024-09-02 12:30:29 +0200

    starter and pieces

---
+++ src/pieces/Pawn.rs
@@ -0,0 +1,18 @@
+use crate::util::color::Color;
+use crate::util::position::Position;
+
+pub(crate) struct Pawn {
+    pub(crate) color: Color,
+    pub(crate) position: Position,
+    pub(crate) has_moved: bool,
+}
+
+impl Pawn{
+    pub(crate) fn new(color: Color, position: Position) -> Self {
+        Self {
+            color,
+            position,
+            has_moved: false,
+        }
+    }
+}
