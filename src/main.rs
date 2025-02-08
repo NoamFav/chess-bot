0d739b7b96ec5884a5508233f54a5f664e9868a3
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-02-08 16:52:37 +0100

    auto-commit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use util::color::Color;
-use util::position::Position;
 use crate::util::board::Board;
 use crate::util::pieces;
+use util::color::Color;
+use util::position::Position;
 
 mod util {
-    pub mod color;
-    pub mod position;
     pub mod board;
+    pub mod color;
     pub mod pieces;
+    pub mod position;
 }
 
-fn main() {
+pub mod thor;
 
+fn main() {
     let _rook = pieces::Rook::new(Color::black(), Position::new('a', '1'));
     let _pawn = pieces::Pawn::new(Color::white(), Position::new('a', '2'));
     let _knight = pieces::Knight::new(Color::black(), Position::new('a', '3'));
     let _bishop = pieces::Bishop::new(Color::white(), Position::new('a', '4'));
     let _king = pieces::King::new(Color::black(), Position::new('a', '5'));
     let _queen = pieces::Queen::new(Color::white(), Position::new('a', '6'));
-
 
     let mut board = Board::new();
     println!("{}", board.render_ascii());
